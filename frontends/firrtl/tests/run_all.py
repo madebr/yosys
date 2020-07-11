f8b4ebaae9a27c717763ef3e3de971d07d9ed420
Anonymous Maarten <anonymous.maarten@gmail.com>
2020-07-11 22:19:20 +0200

    add tests + FAILING REGRESSION TESTS :(

--- frontends/firrtl/tests/run_all.py
+++ frontends/firrtl/tests/run_all.py
@@ -2,6 +2,7 @@ import argparse
 import os
 from pathlib import Path
 import subprocess
+import time
 
 test_dir = Path(__file__).resolve().parent
 
@@ -16,5 +17,8 @@ for root, _, files in os.walk(test_dir):
 		if not file.endswith(".fir"):
 			continue
 		src = test_dir / root / file
+		start = time.time()
 		rc = subprocess.call([str(yosys), "-p", "read_firrtl {}".format(src)], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-		print(src, "OK" if rc == 0 else "FAIL")
+		stop = time.time()
+		ms = (stop - start) * 1000
+		print(src.relative_to(test_dir), "{:.03f}ms".format(ms), "OK" if rc == 0 else "FAIL")
